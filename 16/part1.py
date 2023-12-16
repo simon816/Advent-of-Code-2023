ea8cfaaa4dc40a864e323aef6afe031ea7e42ed3
simon816 <commits@simon816.com>
2023-12-16 06:21:22 +0000

    Day 16

---
+++ 16/part1.py
@@ -0,0 +1,79 @@
+import sys
+
+grid = []
+for line in sys.stdin.readlines():
+    grid.append(line.strip())
+
+beams = set(((0, 0, 'E'),))
+seen = set()
+energised = set()
+while len(beams):
+    new_beams = set()
+    for beam in beams:
+        if beam in seen:
+            continue
+        seen.add(beam)
+        r, c, dir = beam
+        if r < 0 or r > len(grid) - 1 or c < 0 or c > len(grid[r]) - 1:
+            continue
+        energised.add((r, c))
+        tile = grid[r][c]
+        if tile == '.':
+            if dir == 'N':
+                r -= 1
+            elif dir == 'E':
+                c += 1
+            elif dir == 'S':
+                r += 1
+            elif dir == 'W':
+                c -= 1
+        elif tile == '|':
+            if dir in 'EW':
+                new_beams.add((r - 1, c, 'N'))
+                r += 1
+                dir = 'S'
+            elif dir == 'N':
+                r -= 1
+            elif dir == 'S':
+                r += 1
+        elif tile == '-':
+            if dir in 'NS':
+                new_beams.add((r, c + 1, 'E'))
+                c -= 1
+                dir = 'W'
+            elif dir == 'E':
+                c += 1
+            elif dir == 'W':
+                c -= 1
+        elif tile == '/':
+            if dir == 'N':
+                dir = 'E'
+                c += 1
+            elif dir == 'E':
+                dir = 'N'
+                r -= 1
+            elif dir == 'S':
+                dir = 'W'
+                c -= 1
+            elif dir == 'W':
+                dir = 'S'
+                r += 1
+        elif tile == '\\':
+            if dir == 'N':
+                dir = 'W'
+                c -= 1
+            elif dir == 'E':
+                dir = 'S'
+                r += 1
+            elif dir == 'S':
+                dir = 'E'
+                c += 1
+            elif dir == 'W':
+                dir = 'N'
+                r -= 1
+        else:
+            assert False
+        new_beams.add((r, c, dir))
+    beams = new_beams
+
+print(len(energised))
